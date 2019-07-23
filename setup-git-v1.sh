eda12942e2937749564898bb7b10febf580c7825
fishydumpling <niko.lipp@gmail.com>
2019-07-23 15:56:20 -0400

    first commit with budgie scripts

---
+++ setup-git-v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# User		:	Nicholas Lippincott
+# Email 	:	niko.lipp@gmail.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "fishydumpling"
+git config --global user.email "niko.lipp@gmail.com"
+sudo git config --system core.editor vim
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
